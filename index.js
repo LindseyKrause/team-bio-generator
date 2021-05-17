43da1c152da570b8efae7faf70c2fe37f4ed0681
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-05-16 18:21:34 -0700

    initial set-up inquirer, first test, psuedocode

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+
+//Prompt User 
+inquirer
+.prompt([
+{
+    type: 'input',
+    name: 'employeename',
+    message: 'What is your name?'
+},
+
+])
+.then(function (data) {
+    console.log("Employee name should appear after this");
+console.log(data.employeename);
+});
+
+
+
+//Using .then put data into a function that will write an HTML file (fs.write file)
+//Make separate files for writeHtml().js, 
