1e6eea8e509f4f0c9c8eddd53f90a43cae170b1d
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-05-21 20:30:08 -0700

    add manager functionality

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateHtml = require('./src/html-template');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const teamArray = [];
 
+function addManager() {
 //Prompt User 
 inquirer
 .prompt([
 {
     type: 'input',
-    name: 'employeename',
-    message: 'What is your name?'
+    name: 'managerName',
+    message: 'What is the manager\'s name?'
 },
 
+{
+    type: 'input',
+    name: 'managerId',
+    message: 'What is your  manager\'s id?'
+},
+{
+    type: 'input',
+    name: 'managerEmail',
+    message: 'What is your  manager\'s email?'
+},
+{
+    type: 'input',
+    name: 'managerOfficeNumber',
+    message: 'What is your  manager\'s office number?'
+},
 ])
-.then(function (data) {
-    console.log("Employee name should appear after this");
-console.log(data.employeename);
+.then(data => {
+    console.log("Manager name should appear after this");
+console.log(data);
+const manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerOfficeNumber);
+teamArray.push(manager);
 });
-
-
+};
+addManager();
+//add team member
+//manager, intern, engineer, 
+//build them
+//store in array
+//send array to build HTML. 
 
 //Using .then put data into a function that will write an HTML file (fs.write file)
 //Make separate files for writeHtml().js, 
